6cf225064af11e90785587b7977377c27c0142bb
Kenji Saito <k.saito.1985@gmail.com>
2016-03-06 11:20:08 -0800

    updated title and codes.

--- sketches/theme/procedural-mesh/cube2/app.js
+++ sketches/theme/procedural-mesh/cube2/app.js
@@ -63,9 +63,9 @@ scene = new THREE.Scene();
 })();
 
 function setComponent(){
-    var title = 'Cube with BufferGeometry';
+    var title = 'Cube2 with BufferGeometry';
     var caption = '';
-    var url = 'https://github.com/kenjiSpecial/webgl-sketch-dojo/tree/master/sketches/theme/procedural-mesh/cube';
+    var url = 'https://github.com/kenjiSpecial/webgl-sketch-dojo/tree/master/sketches/theme/procedural-mesh/cube2';
 
     wrapper = createCaption(title, caption, url);
     wrapper.style.width = (window.innerWidth/2 - 50) + "px";
