40a110dfb79cff579e28687fe628a21390ddb4f1
Kenji Saito <k.saito.1985@gmail.com>
2016-04-30 20:52:01 -0700

    update node/build.js

--- node/build.js
+++ node/build.js
@@ -12,7 +12,7 @@ var b = browserify();
 b.add(__dirname + '/../main.js');
 b.transform(babelify.configure({presets: ["es2015"]}));
 b.transform(glslify);
-b.transform({global: true}, 'uglifyify')
 b.transform(aliasify.configure({aliases : {"vendors" : "./vendors/"}}));
+b.transform({global: true}, 'uglifyify');
 
 var o = b.bundle().pipe(fs.createWriteStream(__dirname + '/../example/bundle.js'));
